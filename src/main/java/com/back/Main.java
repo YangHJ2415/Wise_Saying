ec55eb10d4beff00d3526f2eeb53e6370effadab
yhj <guswns2415@naver.com>
2025-05-23 14:08:56 +0900

    5단계 구현 완료

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        WiseSaying wiseSaying = null;
+        WiseSaying[] wiseSayings = new WiseSaying[100];
+        int wiseSayingsLastIndex = -1;
+
         int lastId = 0;
 
         System.out.println("== 명언 앱 ==");
@@ -23,19 +25,24 @@ public class Main {
                 System.out.print("작가 : ");
                 String wiseSayingAuthor = scanner.nextLine().trim();
 
-                int id = ++lastId;
 
-                wiseSaying = new WiseSaying();
+                int id = ++lastId;
+                WiseSaying wiseSaying = new WiseSaying();
                 wiseSaying.id = id;
                 wiseSaying.content = wiseSayingContent;
                 wiseSaying.author = wiseSayingAuthor;
+                wiseSayings[++wiseSayingsLastIndex] = wiseSaying;
 
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+                System.out.printf("%d번 명언이 등록되었습니다.%n", id);
             } else if (cmd.equals("목록")){
-                if(wiseSaying == null) continue; // 명언이 등록되지 않았을 때
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("==========================");
-                System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.content,wiseSaying.author));
+
+                for(int i=wiseSayingsLastIndex; i>= 0; i--){
+                    WiseSaying wiseSaying = wiseSayings[i];
+
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("==========================");
+                    System.out.printf("%d / %s / %s%n", wiseSaying.id, wiseSaying.content,wiseSaying.author);
+                }
             }
         }
     }
